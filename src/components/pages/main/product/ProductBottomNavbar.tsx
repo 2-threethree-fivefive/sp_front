a23c1262eaf458cc79333d6e0ac0c14accef9e6f
sshh121 <lc307715@gmail.com>
2024-09-08 00:35:51 +0900

    style: 상품 상세 페이지 drawer 퍼블리싱

--- src/components/pages/main/product/ProductBottomNavbar.tsx
+++ src/components/pages/main/product/ProductBottomNavbar.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import LikeButton from '@/components/ui/LikeButton';
 import { Gift } from 'lucide-react';
 import { Drawer, DrawerTrigger } from '@/components/ui/drawer';
 import ProductBottomNavbarDrawer from './ProductBottomNavbarDrawer';
 
 function ProductBottomNavbar() {
+  const [type, setType] = useState<number>(0);
+  const handleClick = (index: number) => {
+    setType(index);
+  };
   return (
     <ul className="w-full flex items-center text-center h-14 fixed bottom-0 left-0 z-10 bg-white font-semibold">
       <li className="w-[15%] h-full border flex justify-center items-center">
@@ -12,15 +17,21 @@ function ProductBottomNavbar() {
       </li>
       <Drawer>
         <DrawerTrigger className="w-full h-full flex">
-          <li className="w-[40%] h-full border flex gap-1 justify-center items-center">
+          <li
+            className="w-[40%] h-full border flex gap-1 justify-center items-center"
+            onClick={() => handleClick(1)}
+          >
             <Gift width={20} />
             선물하기
           </li>
-          <li className="w-[65%] h-full border flex justify-center items-center text-white bg-starbucks-red">
+          <li
+            className="w-[65%] h-full border flex justify-center items-center text-white bg-starbucks-red"
+            onClick={() => handleClick(2)}
+          >
             구매하기
           </li>
         </DrawerTrigger>
-        <ProductBottomNavbarDrawer />
+        <ProductBottomNavbarDrawer type={type} />
       </Drawer>
     </ul>
   );
